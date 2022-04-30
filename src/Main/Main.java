a2bedffca09f1290b26654e7f3b95c6323158d9b
Gustavo Cercal <gustavocercal13@gmail.com>
2022-04-30 18:32:52 -0300

    Atualização

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         BuscaFilmes = new BuscaFilmes();
         BuscaFilmes.setVisible(true);
-
+        System.out.println("Ok!");
     }
 
 
